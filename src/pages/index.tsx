c2788bd61393bfaeb356ab4be39306ce0c4d0354
Francisco Delgado <franny285@gmail.com>
2023-07-26 19:39:21 -0500

    white 20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -178,7 +178,7 @@ export default function Home() {
                                     scrollPercentage <= breakpoint &&
                                     scrollPercentage >= prevInnerBreakpoint &&
                                     !onLastSentence,
-                                  "text-white/30 lg:text-white/10":
+                                  "text-white/20 lg:text-white/10":
                                     scrollPercentage > breakpoint &&
                                     !onLastSentence,
                                 })}
@@ -198,7 +198,7 @@ export default function Home() {
                               scrollPercentage < breakpoint &&
                               scrollPercentage >= prevBreakpoint &&
                               !onLastSentence,
-                            "text-white/30 lg:text-white/10":
+                            "text-white/20 lg:text-white/10":
                               scrollPercentage > breakpoint && !onLastSentence,
                           })}
                         >
